59528273ee0c1d84f2ad1e2d48a01a853eb7cd1d
Ayush H <jqghebri@gmail.com>
2024-08-21 00:56:55 +0530

    creation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
+  darkMode: "class", //media is used if u want it based on the computer system's dark mode preference
   theme: {
     extend: {},
   },
